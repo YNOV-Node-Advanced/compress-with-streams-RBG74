0e0675184ab15c68610d9cd2988a23dfb76a4ddf
RBG74 <remy.bernardgranger@gmail.com>
2018-05-31 11:42:51 +0200

    Done

---
+++ compress.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const zlib = require("zlib");
+
+async function compress(input, output) {
+    return new Promise((resolve, reject) => {
+        const gzip = zlib.createGzip();
+
+        let inputStream = fs.createReadStream(input);
+        const outputStream = fs.createWriteStream(output);
+        outputStream.on("close", () => {
+            resolve(outputStream.bytesWritten);
+        });
+
+        inputStream.on("error", err => {
+            reject(err);
+        });
+        outputStream.on("error", err => {
+            reject(err);
+        });
+
+        inputStream.pipe(gzip).pipe(outputStream);
+    });
+}
+
+compress("input.txt", "output.txt")
+    .then(result => console.log("Bytes written: ", result))
+    .catch(err => console.error(err));
